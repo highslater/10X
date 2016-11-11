f26a9da91c85981fc7ba3fb50e07998ef42e8467
highslater <highslater@hotmail.com>
2016-11-10 22:56:10 -0500

    added comments

--- Rails5_The_Tour/videoblog/app/controllers/questions_controller.rb
+++ Rails5_The_Tour/videoblog/app/controllers/questions_controller.rb
@@ -1,2 +1,20 @@
+#videoblog/app/controllers/questions_controller.rb
 class QuestionsController < ApplicationController
+  before_action :set_video
+
+  def create
+    @video.questions.create!(questions_params)
+    redirect_to @video
+  end
+
+  private
+
+  def set_video
+    @video = Video.find(params[:video_id])
+  end
+
+  def questions_params
+    params.required(:question).permit(:body)
+  end
+
 end
