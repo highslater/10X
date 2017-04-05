90be36d0e395409c01b52f78ba1ad7001bc88341
highslater <highslater@hotmail.com>
2017-04-05 12:57:18 -0400

    commit

--- _12_in_12/week_06/todo/config/routes.rb
+++ _12_in_12/week_06/todo/config/routes.rb
@@ -1,10 +1,15 @@
 Rails.application.routes.draw do
 
-  root to: 'todo_lists#index'
+  root "todo_lists#index"
 
   resources :todo_lists do
-    resources :todo_items
+    resources :todo_items do
+      member do
+        patch :complete
+      end
+    end
   end
+
 
 end
 
@@ -12,30 +17,29 @@ end
 =begin
 
 
-rake routes
+rails routes
+                      Prefix Verb   URI Pattern                                                 Controller#Action
 
-                  Prefix Verb   URI Pattern                                             Controller#Action
+                        root GET    /                                                           todo_lists#index
 
-                    root GET    /                                                       todo_lists#index
-
-    todo_list_todo_items GET    /todo_lists/:todo_list_id/todo_items(.:format)          todo_items#index
-                         POST   /todo_lists/:todo_list_id/todo_items(.:format)          todo_items#create
- new_todo_list_todo_item GET    /todo_lists/:todo_list_id/todo_items/new(.:format)      todo_items#new
-edit_todo_list_todo_item GET    /todo_lists/:todo_list_id/todo_items/:id/edit(.:format) todo_items#edit
-     todo_list_todo_item GET    /todo_lists/:todo_list_id/todo_items/:id(.:format)      todo_items#show
-                         PATCH  /todo_lists/:todo_list_id/todo_items/:id(.:format)      todo_items#update
-                         PUT    /todo_lists/:todo_list_id/todo_items/:id(.:format)      todo_items#update
-                         DELETE /todo_lists/:todo_list_id/todo_items/:id(.:format)      todo_items#destroy
-                         
-              todo_lists GET    /todo_lists(.:format)                                   todo_lists#index
-                         POST   /todo_lists(.:format)                                   todo_lists#create
-           new_todo_list GET    /todo_lists/new(.:format)                               todo_lists#new
-          edit_todo_list GET    /todo_lists/:id/edit(.:format)                          todo_lists#edit
-               todo_list GET    /todo_lists/:id(.:format)                               todo_lists#show
-                         PATCH  /todo_lists/:id(.:format)                               todo_lists#update
-                         PUT    /todo_lists/:id(.:format)                               todo_lists#update
-                         DELETE /todo_lists/:id(.:format)                               todo_lists#destroy
-
+complete_todo_list_todo_item PATCH  /todo_lists/:todo_list_id/todo_items/:id/complete(.:format) todo_items#complete
+        todo_list_todo_items GET    /todo_lists/:todo_list_id/todo_items(.:format)              todo_items#index
+                             POST   /todo_lists/:todo_list_id/todo_items(.:format)              todo_items#create
+     new_todo_list_todo_item GET    /todo_lists/:todo_list_id/todo_items/new(.:format)          todo_items#new
+    edit_todo_list_todo_item GET    /todo_lists/:todo_list_id/todo_items/:id/edit(.:format)     todo_items#edit
+         todo_list_todo_item GET    /todo_lists/:todo_list_id/todo_items/:id(.:format)          todo_items#show
+                             PATCH  /todo_lists/:todo_list_id/todo_items/:id(.:format)          todo_items#update
+                             PUT    /todo_lists/:todo_list_id/todo_items/:id(.:format)          todo_items#update
+                             DELETE /todo_lists/:todo_list_id/todo_items/:id(.:format)          todo_items#destroy
+                             
+                  todo_lists GET    /todo_lists(.:format)                                       todo_lists#index
+                             POST   /todo_lists(.:format)                                       todo_lists#create
+               new_todo_list GET    /todo_lists/new(.:format)                                   todo_lists#new
+              edit_todo_list GET    /todo_lists/:id/edit(.:format)                              todo_lists#edit
+                   todo_list GET    /todo_lists/:id(.:format)                                   todo_lists#show
+                             PATCH  /todo_lists/:id(.:format)                                   todo_lists#update
+                             PUT    /todo_lists/:id(.:format)                                   todo_lists#update
+                             DELETE /todo_lists/:id(.:format)                                   todo_lists#destroy
 
 
 =end
