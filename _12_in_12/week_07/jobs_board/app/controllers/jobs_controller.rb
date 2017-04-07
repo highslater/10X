97d7dd6901735ae432933cdae9c7808ad6e99391
highslater <highslater@hotmail.com>
2017-04-07 15:27:08 -0400

    commit

--- _12_in_12/week_07/jobs_board/app/controllers/jobs_controller.rb
+++ _12_in_12/week_07/jobs_board/app/controllers/jobs_controller.rb
@@ -4,7 +4,12 @@ class JobsController < ApplicationController
 
 
   def index
-    @jobs = Job.all.order("created_at DESC")
+    if params[:category].blank?
+      @jobs = Job.all.order("created_at DESC")
+    else
+      @category_id = Category.find_by(name: params[:category]).id
+      @jobs = Job.where(category_id: @category_id).order("created_at DESC")
+    end
   end
 
   def show
@@ -28,6 +33,11 @@ class JobsController < ApplicationController
   end
 
   def update
+    if @job.update(jobs_params)
+      redirect_to @job
+    else
+      render "Edit"
+    end
   end
 
   def destroy
@@ -39,7 +49,7 @@ class JobsController < ApplicationController
   private
 
   def jobs_params
-    params.require(:job).permit(:title, :description, :company, :url)
+    params.require(:job).permit(:title, :description, :company, :url, :category_id)
   end
 
   def find_job
--- _12_in_12/week_07/jobs_board/db/seeds.rb
+++ _12_in_12/week_07/jobs_board/db/seeds.rb
@@ -5,3 +5,53 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+# Job(
+#   title: string,
+#   description: text,
+#   company: string, url: string,
+#   category_id: integer
+# )
+Job.create(
+  [
+    {
+      title: 'Full Stack Web Developer', description: 'Senior Level',
+      company: 'Twitter', category_id: 1, url: '#'
+    },
+    {
+      title: 'Full Stack Web Developer', description: 'Junior Level',
+      company: 'Twitter', category_id: 2, url: '#'
+    },
+    {
+      title: 'Front End Web Developer', description: 'Senior Level',
+      company: 'FaceBook', category_id: 3, url: '#'
+    },
+    {
+      title: 'Front End Web Developer', description: 'Junior Level',
+      company: 'FaceBook', category_id: 4, url: '#'
+    },
+  ]
+)
+
+
+# Category(
+#     id: integer,
+#     name: string,
+#     )
+Category.create(
+  [
+    {
+      name: 'Full Time'
+    },
+    {
+      name: 'Part Time'
+    },
+    {
+      name: 'FreeLance'
+    },
+    {
+      name: 'Consulting'
+    },
+  ]
+)
