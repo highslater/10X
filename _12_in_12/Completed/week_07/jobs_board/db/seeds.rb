07d48a41689bc2e222de55c479836eeb707f5fa9
highslater <highslater@hotmail.com>
2017-04-08 22:14:19 -0400

    commit

--- _12_in_12/Completed/week_07/jobs_board/db/seeds.rb
+++ _12_in_12/Completed/week_07/jobs_board/db/seeds.rb
@@ -1,44 +1,29 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
 
-
-# Job(
-#   title: string,
-#   description: text,
-#   company: string, url: string,
-#   category_id: integer
-# )
 Job.create(
   [
     {
       title: 'Full Stack Web Developer', description: 'Senior Level',
-      company: 'Twitter', category_id: 1, url: '#'
+      company: 'Twitter', category_id: 1, url: 'https://twitter.com'
     },
     {
       title: 'Full Stack Web Developer', description: 'Junior Level',
-      company: 'Twitter', category_id: 2, url: '#'
+      company: 'Twitter', category_id: 2, url: 'https://twitter.com'
     },
     {
       title: 'Front End Web Developer', description: 'Senior Level',
-      company: 'FaceBook', category_id: 3, url: '#'
+      company: 'FaceBook', category_id: 3, url: 'https://www.facebook.com/'
     },
     {
       title: 'Front End Web Developer', description: 'Junior Level',
-      company: 'FaceBook', category_id: 4, url: '#'
+      company: 'FaceBook', category_id: 4, url: 'https://www.facebook.com/'
     },
   ]
 )
 
 
-# Category(
-#     id: integer,
-#     name: string,
-#     )
 Category.create(
   [
     {
